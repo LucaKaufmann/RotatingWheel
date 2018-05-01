380496764aa147ef5dd70a705d7f69bf6905a0cd
Luca <lucakaufmann92@gmail.com>
2018-05-01 13:28:50 +0300

    Initial Commit

---
+++ RotatingWheel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RotatingWheel
+//
+//  Created by Luca Kaufmann on 01/05/2018.
+//  Copyright © 2018 Luca Kaufmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
